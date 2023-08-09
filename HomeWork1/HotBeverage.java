c7d31e839d84d485706e622871a923b1d404f893
Nikita <kindpavlov@yandex.ru>
2023-08-09 13:35:59 +0300

    + условие задачи

--- HomeWork1/HotBeverage.java
+++ HomeWork1/HotBeverage.java
@@ -1,6 +1,8 @@
 package HomeWork1;
 
 public class HotBeverage extends Beverage {
+  protected int temperature;
+
   public HotBeverage(String name, double price, double volume, int temperature) {
     super(name, price, volume);
     this.temperature = temperature;
