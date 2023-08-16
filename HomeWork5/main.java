b69b07fc7bc324d6a36bd8fd920916f1f1873394
Nikita_Pavlov <126702146+NikitaPavloB@users.noreply.github.com>
2023-08-16 07:35:26 +0300

    Задача 5

---
+++ HomeWork5/main.java
@@ -0,0 +1,12 @@
+package HomeWork5;
+
+import Seminar.Seminar5.view.UserView;
+
+public class main {
+
+  public static void main(String[] args) {
+
+    UserView userView = new UserView();
+    userView.addUser();
+  }
+}
